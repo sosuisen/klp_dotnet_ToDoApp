71f8c090bf2b5d2439bcd18a4372a91c1781519f
sosuisen <hidekazu.kubota@gmail.com>
2023-12-21 01:59:43 +0900

    プロジェクト ファイルを追加します。

---
+++ ToDoApp/ToDoViewModel.cs
@@ -0,0 +1,30 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToDoApp
+{
+    internal partial class ToDoViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private List<RowItems> _listViewRows;
+
+        public ToDoViewModel()
+        {
+            _listViewRows = new List<RowItems>();
+            _listViewRows.Add(new RowItems("Buy milk", DateTime.Now));
+            _listViewRows.Add(new RowItems("Buy PC", new DateTime(2023, 12, 24), true));
+            _listViewRows.Add(new RowItems("Buy chocolate", new DateTime(2024, 2, 14), true));           
+        }   
+    }
+
+    public partial class RowItems(string name, DateTime deadline, bool completed=false): ObservableObject
+    {
+        [ObservableProperty]
+        private string _name = name;
+
+        [ObservableProperty]
+        private DateTime _deadline = deadline;
+        
+        [ObservableProperty]
+        private bool _completed = completed;
+    }
+}
