9dacfacc0fa253a2148ec7dffdaf59930753e6d1
sosuisen <hidekazu.kubota@gmail.com>
2024-02-04 01:08:04 +0900

    fix: 2way binding for ToDo properties

--- ToDoApp/ToDoModel.cs
+++ ToDoApp/ToDoModel.cs
@@ -1,18 +1,30 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace ToDoApp
 {
     internal class ToDoModel
     {
         public List<ToDo> ToDos { get; set; } = [
-            new ToDo("Buy milk", DateTime.Now),
-            new ToDo("Buy new PC", new DateTime(2023, 12, 24), true),
-            new ToDo("Buy chocolate", new DateTime(2024, 2, 14), true)
+            new ToDo(0, "Buy milk", DateTime.Now),
+            new ToDo(1, "Buy new PC", new DateTime(2023, 12, 24), true),
+            new ToDo(2, "Buy chocolate", new DateTime(2024, 2, 14), true)
         ];
+
+        public void UpdateName(ToDo todo, string name) => Debug.WriteLine($"Name has been updated to {name} in ToDo#{todo.Id}");
+        public void UpdateDeadline(ToDo todo, DateTime deadline) => Debug.WriteLine($"Deadline has been updated to {deadline} in ToDo#{todo.Id}");
+        public void UpdateCompleted(ToDo todo, bool completed) => Debug.WriteLine($"Completed has been updated to {completed} in ToDo#{todo.Id}");
     }
 
-    internal class ToDo(string name, DateTime deadline, bool completed = false)
+    partial class ToDo(int id, string name, DateTime deadline, bool completed = false) : ObservableObject
     {
-        public string Name { get; set; } = name;
-        public DateTime Deadline { get; set; } = deadline;
-        public bool Completed { get; set; } = completed;
+        public int Id { get; set; } = id;
+        [ObservableProperty]
+        private string _name = name;
+        [ObservableProperty]
+        private DateTime _deadline = deadline;
+        [ObservableProperty]
+        private bool _completed = completed;
     }
 }
--- ToDoApp/ToDoViewModel.cs
+++ ToDoApp/ToDoViewModel.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using static ToDoApp.ToDoModel;
 
 namespace ToDoApp
 {
@@ -8,12 +9,41 @@ namespace ToDoApp
     {
         private ToDoModel _model;
 
-        public ObservableCollection<ToDo> ListViewRows { get; set; }
+        [ObservableProperty]
+        private List<ToDo> _listViewRows;
+
+        partial void OnListViewRowsChanged(List<ToDo> value)
+        {
+            Debug.WriteLine(value);
+        }
 
         public ToDoViewModel()
         {
              _model = new();
-             ListViewRows = new(_model.ToDos);
-        }   
+             _listViewRows = new(_model.ToDos);
+            foreach (var todo in _listViewRows)
+            {
+                todo.PropertyChanged += ToDoPropertyChanged;
+            }
+        }
+
+        private void ToDoPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (sender is ToDo todo)
+            {
+                switch (e.PropertyName)
+                {
+                    case nameof(ToDo.Name):
+                        _model.UpdateName(todo, todo.Name);
+                        break;
+                    case nameof(ToDo.Deadline):
+                        _model.UpdateDeadline(todo, todo.Deadline);
+                        break;
+                    case nameof(ToDo.Completed):
+                        _model.UpdateCompleted(todo, todo.Completed);
+                        break;
+                }
+            }
+        }
     }
 }
